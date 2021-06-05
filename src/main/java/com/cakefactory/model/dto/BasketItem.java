406b720d014d0eac9cbed9f8df129dabd39b119d
Najeeb Arif <NajeebArif786@outlook.com>
2021-06-06 01:58:55 +0530

    Adding font-awesome

--- src/main/java/com/cakefactory/model/dto/BasketItem.java
+++ src/main/java/com/cakefactory/model/dto/BasketItem.java
@@ -2,10 +2,16 @@ package com.cakefactory.model.dto;
 
 import lombok.Data;
 
+import java.math.BigDecimal;
+
 @Data
 public class BasketItem {
 
     private ItemDto itemDto;
     private Integer qty;
+
+    public BigDecimal totalPrice(){
+        return itemDto.getPrice().multiply(BigDecimal.valueOf(qty));
+    }
 
 }
