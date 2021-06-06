6a2680ae2ba1cc49530e897241c2c06ec6004fc9
Najeeb Arif <NajeebArif786@outlook.com>
2021-06-07 00:11:23 +0530

    Update V3__add_account.sql

--- src/main/resources/db/migration/V3__add_account.sql
+++ src/main/resources/db/migration/V3__add_account.sql
@@ -1,24 +1,28 @@
 CREATE TABLE users (
-    id Number Not Null primary key,
+    id SERIAL PRIMARY KEY,
     first_name VARCHAR(255) NOT NULL,
     last_name VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL unique,
-    age number NOT NULL,
-    phone_number varchar2(100),
+    age SMALLINT NOT NULL,
+    phone_number varchar(100),
     username VARCHAR(255) NOT NULL);
 
 CREATE TABLE address (
-    id Number Not Null primary key,
-    user_id Number NOT NULL,
+    id SERIAL PRIMARY KEY,
+    user_id INT NOT NULL,
     address_line1 VARCHAR(255) NOT NULL,
     address_line2 VARCHAR(255),
+    city VARCHAR(255),
     postcode VARCHAR(10) NOT NULL,
-    Foreign key user_id REFERENCES users (id) );
+    CONSTRAINT fk_addr_user
+    Foreign key (user_id) REFERENCES users (id) ON DELETE CASCADE
+);
 
-create table USER_CREDENTIALS(
-    id number not null primary key,
-    email varchar2(255) not null unique,
-    password varchar2(255) not null,
+create table USER_CREDENTIALS (
+    id SERIAL PRIMARY KEY,
+    email varchar(255) NOT NULL,
+    password varchar(255) NOT NULL,
     enabled boolean,
-    foreign key email references users (email)
+    constraint fk_user_cred_user
+    foreign key (email) references users (email)
 );
